3bdaf608a484a2f4aaecae7e9379279213b2ba9e
bryant tee <bryantt23@gmail.com>
2015-01-25 02:02:18 -0800

    25

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,8 +1,29 @@
 class DriversController < ApplicationController
   def index
+    @drivers = Driver.all 
   end
 
   def new
     @driver = Driver.new
   end
+    
+  def create
+#     prints out parameters to the screen
+#   can see hash & key
+    # render text: params.inspect
+    # do this to know which params to require below
+
+    @driver = Driver.new(    
+    params.require(:driver).permit(:first_name, :last_name)
+    )
+    
+    if @driver.save
+      redirect_to drivers_url
+    else
+      render 'new'
+    end
+  end
+
+
+
 end
